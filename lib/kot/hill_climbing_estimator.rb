d4e467a4ed23d74517d89c74443a65acf322f3d1
Adam Watkins <adam@stupidpupil.co.uk>
2018-08-25 16:10:20 +0100

    Some fixes and some clarification

--- lib/kot/hill_climbing_estimator.rb
+++ lib/kot/hill_climbing_estimator.rb
@@ -12,9 +12,33 @@ module Kot
       last_response ? est_theta + ((max_b - est_theta) / 2.0) : est_theta - ((est_theta - min_b) / 2.0)
     end
 
+
+    def estimate_iteration(best_theta, max_ll, lower_bound, upper_bound, responses, items)
+
+        step_size = (upper_bound - lower_bound) /  10
+        intervals = (lower_bound...upper_bound).step(step_size.abs)
+
+        intervals.each do |ii|
+
+          ll = ItemResponseTheory.log_likelihood(ii, responses, items)
+
+          if ll > max_ll
+            max_ll = ll
+
+            #TODO - precision-based early exit
+            best_theta = ii
+          else
+            lower_bound = best_theta - step_size.abs
+            upper_bound = ii
+            break
+          end
+
+        end
+
+        return best_theta, max_ll, lower_bound, upper_bound
+    end
+
     def estimate(responses:[], items:[], all_items:[], est_theta:0.0)
-
-
       if responses.uniq.count == 1
         raise ArgumentError.new("Responses are all #{responses.first} but missing all_items argument") if all_items.empty?
         return dodd(est_theta:est_theta, items:all_items, last_response:responses.last)
@@ -28,30 +52,11 @@ module Kot
       best_theta = - Float::INFINITY
       max_ll = - Float::INFINITY
 
+      old_best_theta = best_theta
+
       10.times do
-
-        step_size = (upper_bound - lower_bound) /  10
-        intervals = (lower_bound...upper_bound).step(step_size)
-
-        intervals.each do |ii|
-
-          ll = ItemResponseTheory.log_likelihood(ii, responses, items)
-
-
-          if ll > max_ll
-            max_ll = ll
-
-            #TODO - precision-based early exit
-            best_theta = ii
-          else
-            lower_bound = best_theta - step_size
-            upper_bound = [ii, best_theta+step_size].max #HACK
-            break
-          end
-
-        end
-
-      # End of hillclimb
+        best_theta, max_ll, lower_bound, upper_bound = 
+          estimate_iteration(best_theta, max_ll, lower_bound, upper_bound, responses, items)
       end
 
       return best_theta
