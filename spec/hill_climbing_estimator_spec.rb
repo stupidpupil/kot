7fac323bb633915d0e1207083b5115bcfff0e0d9
Adam Watkins <adam@stupidpupil.co.uk>
2018-08-25 16:25:27 +0100

    Add another test for Hill Climbing

--- spec/hill_climbing_estimator_spec.rb
+++ spec/hill_climbing_estimator_spec.rb
@@ -21,6 +21,20 @@ describe Kot::HillClimbingEstimator do
     end
   end
 
+
+  describe 'when given items [b:0.5-x, b:0.5, b:0.5, b:0.5+x] and responses [true, true, false, false]' do
+
+    it 'should give an estimated theta of very close to 0.5' do
+
+      d = rand()
+
+      Kot::HillClimbingEstimator.new.estimate(
+        items:Item4PL[{b:0.5-d}, {b:0.5}, {b:0.5}, {b:0.5+d}], 
+        responses:[true, true, false, false]
+      ).should.be.close 0.5,0.01
+    end
+  end
+
   describe 'when given all items [b:0.5] and responses [true]' do
 
     # Assumes an prior estimated theta of 0.0 and uses Dodd's method
